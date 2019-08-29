50610d3ed11ab474fa677c1847e0d929e8e7f029
Unknown <hackerxse@hotmail.com>
2019-08-29 10:11:55 +1000

    Thrust

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,12 +6,24 @@ namespace HackedDesign
 {
     public class PlayerController : MonoBehaviour
     {
+
+
         [SerializeField]
         private float turnRate = 90.0f;
+
+        [SerializeField]
+        private Rigidbody2D rb;
+
+        [SerializeField]
+        private float maxThrust = 2.0f;
+
+        [SerializeField]
+        private float currentThrust = 0.0f;
+
         // Start is called before the first frame update
         void Start()
         {
-
+            //rb = GetComponent<Rigidbody>();
         }
 
         // Update is called once per frame
@@ -22,11 +34,17 @@ namespace HackedDesign
 
         public void UpdateMovement()
         {
-            float turn = -1.0f * UnityEngine.Input.GetAxis ("Horizontal") * turnRate;
-            this.transform.Rotate(new Vector3(0,0,turn * Time.deltaTime));
+            float turn = -1.0f * UnityEngine.Input.GetAxis("Horizontal") * turnRate;
+            this.transform.Rotate(new Vector3(0, 0, turn * Time.deltaTime));
+            float force = UnityEngine.Input.GetAxis("Vertical") * maxThrust * Time.deltaTime;
 
-            
+            rb.drag = (force / maxThrust);
+            rb.AddRelativeForce(new Vector2(0, force), ForceMode2D.Impulse);
 
+            // if (rb.velocity.magnitude < maxThrust)
+            // {
+            //     rb.velocity = rb.velocity.normalized * maxThrust;
+            // }
         }
     }
 }
