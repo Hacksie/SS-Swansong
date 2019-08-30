3df538e3c24cb795751df707be62d3498617dc5f
Unknown <hackerxse@hotmail.com>
2019-08-30 16:30:09 +1000

    Clean radar arrow code

--- Assets/Scripts/RadarArrow.cs
+++ Assets/Scripts/RadarArrow.cs
@@ -10,24 +10,29 @@ namespace HackedDesign
         {
             if (Game.Instance.state != GameState.PLAYING)
             {
-                this.gameObject.SetActive(false);
+                if (this.gameObject.activeInHierarchy)
+                {
+                    this.gameObject.SetActive(false);
+                }
                 return;
             }
 
             if (Game.Instance.highestRadar != null)
             {
-
-                this.gameObject.SetActive(true);
-                Vector3 direction = (Game.Instance.highestRadar.transform.position - Game.Instance.camera.transform.position).normalized * magnitude;
-                this.transform.position = Game.Instance.camera.transform.position + direction;
-
-                //FIXME: Speed this up
-                var angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg - 90;
-                transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+                if (!this.gameObject.activeInHierarchy)
+                {
+                    this.gameObject.SetActive(true);
+                }
+                Vector2 direction = (Game.Instance.highestRadar.transform.position - Game.Instance.camera.transform.position).normalized * magnitude;
+                this.transform.position = new Vector2(Game.Instance.camera.transform.position.x, Game.Instance.camera.transform.position.y) + direction;
+                transform.up = direction;
             }
             else
             {
-                this.gameObject.SetActive(false);
+                if (this.gameObject.activeInHierarchy)
+                {
+                    this.gameObject.SetActive(false);
+                }
             }
 
         }
